d8a1c71003da3acac9315e354998469165762220
Ajeet Singh <ajeet.cloud@gmail.com>
2023-05-10 21:52:49 +0530

    Added constants

--- src/app/common/constants.ts
+++ src/app/common/constants.ts
@@ -1,5 +1,7 @@
 export const CLIENT_ID = "";
+
 export const CLIENT_SECRET = "";
+
 export const REDIRECT_URI = "http://localhost:4200";
 
 // https://www.googleapis.com/auth/drive.file - See, edit, create, and delete only the specific Google Drive files you use with this app
@@ -16,4 +18,3 @@ export const RESET_ACCESS_TOKEN_INTERVAL_MS = 3360000 // 56 minutes
 export const DRIVE_URL_OF_FILE = "https://drive.google.com/file/d/";
 
 export const FILE_SIZE_5_MB = 5 * 1024 * 1024;
-
